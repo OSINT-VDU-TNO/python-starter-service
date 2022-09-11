e5a2e167de9b29bd59d78a0ef45c912e98383216
Mindaugas Petkvevičius <petkeviciusm@gmail.com>
2022-09-11 11:35:36 +0300

    Fix requirements

--- setup.py
+++ setup.py
@@ -2,10 +2,11 @@ import setuptools
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
-
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
 setuptools.setup(
     name="osint-python-starter-service",
-    version="1.0.2",
+    version="1.0.3",
     author="mindpetk",
     author_email="petkeviciusm@gmail.com",
     description="Python starter service",
@@ -13,6 +14,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/OSINT-VDU-TNO/python-starter-service",
     include_package_data=True,
+    install_requires=required,
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
